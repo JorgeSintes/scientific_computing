3bda5cfd8b09b0091dec859c5e5a13177a5d084f
Jorge Sintes Fernández <77026981+JorgeSintes@users.noreply.github.com>
2021-05-28 18:16:13 +0200

    Normalize global error and make nicer plots

--- part_1/main.m
+++ part_1/main.m
@@ -3,7 +3,7 @@ clc
 close all
 
 tspan = [0 5];
-h = 0.1;
+h = 0.2;
 x0 = 1;
 lambda = -1;
 
@@ -26,8 +26,20 @@ plot(T,e_l)
 plot(T,e_g)
 hold off
 ylabel('errors')
-legend('real solution', 'numerical solution', 'local error', 'global error')
+legend('numerical solution','real solution', 'local error', 'global error')
 limits = ylim;
+
+% set(gcf,'Units','inches');
+% screenposition = get(gcf,'Position');
+% set(gcf,...
+%     'PaperPosition',[0 0 screenposition(3:4)],...
+%     'PaperSize',[screenposition(3:4)]);
+% print -dpdf -painters 1_explicit_0_2
+
+set(gcf,'Units','Inches');
+pos = get(gcf,'Position');
+set(gcf,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+print(gcf,'1_explicit_0_2','-dpdf','-r0')
 
 figure()
 yyaxis left
@@ -44,12 +56,19 @@ plot(T1,e_g1)
 ylim(limits)
 hold off
 ylabel('errors')
-legend('real solution', 'numerical solution', 'local error', 'global error')
+legend('numerical solution','real solution', 'local error', 'global error')
+
+set(gcf,'Units','Inches');
+pos = get(gcf,'Position');
+set(gcf,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+print(gcf,'1_implicit_0_2','-dpdf','-r0')
 
 
 %% Evolution of the error vs. stepsize
+clc
+close all
 tspan = [0 15];
-hs = 0.005:0.005:1;
+hs = 0.005:0.001:0.5;
 
 e_ls_ex = zeros(1,size(hs,2));
 e_gs_ex = zeros(1,size(hs,2));
@@ -62,22 +81,47 @@ for h=hs
     [T,X,X_real,e_l,e_g] = EulerExplicit(@TestEquation, @Exponential, tspan, h, x0, args);
     [T1,X1,X_real1,e_l1,e_g1] = EulerImplicitTest(@Exponential, tspan, h, x0, lambda);
     e_ls_ex(i) = sum(e_l);
-    e_gs_ex(i) = sum(e_g);
+    e_gs_ex(i) = sum(e_g)/size(e_g,2);
     e_ls_im(i) = sum(e_l1);
-    e_gs_im(i) = sum(e_g1);
+    e_gs_im(i) = sum(e_g1)/size(e_g1,2);
 end
 
 figure()
 plot(hs,e_ls_ex)
 hold on
-ax = gca;
-ax.ColorOrderIndex = 1;
-plot(hs,e_gs_ex, '-.')
-
 plot(hs,e_ls_im)
-ax.ColorOrderIndex = 2;
+% ax = gca;
+% ax.ColorOrderIndex = 1;
+legend('explicit', 'implicit')
+
+figure()
+plot(hs,e_gs_ex, '-.')
+hold on
+% ax.ColorOrderIndex = 2;
 plot(hs,e_gs_im, '-.')
 
 xlabel('h')
 ylabel('error')
-legend('explicit - local', 'explicit - global', 'implicit - local', 'implicit - global')
+legend('explicit', 'implicit')
+
+%% Trial
+clc
+close all
+h = hs(1);
+h = 0.00001;
+
+[T,X,X_real,e_l,e_g] = EulerExplicit(@TestEquation, @Exponential, tspan, h, x0, args);
+[T1,X1,X_real1,e_l1,e_g1] = EulerImplicitTest(@Exponential, tspan, h, x0, lambda);
+
+figure()
+plot(T,X)
+hold on
+plot(T,X1)
+plot(T,X_real)
+legend('explicit', 'implicit', 'real')
+sum(e_l)
+sum(e_g)
+sum(e_l1)
+sum(e_g1)
+sum(X-X_real)
+sum(X1-X_real1)
