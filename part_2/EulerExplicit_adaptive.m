ca0b88f532f8e2a4c4187121f353b9e611e6e34a
Jorge Sintes Fernández <77026981+JorgeSintes@users.noreply.github.com>
2021-05-31 01:45:30 +0200

    Added csv creation and final plots in Van der Pol

--- part_2/EulerExplicit_adaptive.m
+++ part_2/EulerExplicit_adaptive.m
@@ -1,4 +1,4 @@
-function [T,X] = EulerExplicit_adaptive(fun,tspan,h0,x0,abstol,reltol,args)
+function [T,X,r_out,h_out,info] = EulerExplicit_adaptive(fun,tspan,h0,x0,abstol,reltol,args)
 
 epstol = 0.8;
 facmin = 0.1;
@@ -12,13 +12,20 @@ x = x0;
 
 T = t0;
 X = x0;
+r_out = [];
+h_out = [];
+info = zeros(1,4);
+
+nfun = 0;
+nstep = 0;
+naccept = 0;
 
 while t < tf
     if (t+h > tf)
         h = tf-t;
     end
     f = feval(fun,t,x,args{:});
-    
+    nfun = nfun + 1;
     AcceptStep = false;
     while ~AcceptStep
         x1 = x + h*f;
@@ -27,7 +34,10 @@ while t < tf
         tm = t + hm;
         xm = x + hm*f;
         fm = feval(fun,tm,xm,args{:});
+        nfun = nfun + 1;
         x1hat = xm + hm*fm;
+        
+        nstep = nstep + 1;
         
         e = abs(x1hat-x1);
         r = max(e./max(abstol, abs(x1hat) .* reltol));
@@ -39,8 +49,18 @@ while t < tf
             
             T = [T,t];
             X = [X,x];
+            r_out = [r_out, r];
+            h_out = [h_out, h];
+            naccept = naccept + 1;
         end
         
         h = max(facmin, min(sqrt(epstol/r), facmax)) * h;
     end
+end
+
+info(1) = nfun;
+info(2) = nstep;
+info(3) = naccept;
+info(4) = nstep - naccept;
+
 end
--- part_2/VanderPol_main.m
+++ part_2/VanderPol_main.m
@@ -3,115 +3,347 @@ clc
 clear
 close all
 
-x0 = [1;1];
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% TIME-STEP SELECTION
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 hs = [0.2 0.1 0.01 0.001];
+slashed_h = [4];
 
+
+h_legend = [];
+for h=hs
+    h_legend = [h_legend, 'h = '+string(h)];
+end
+
+x0 = [1;1];
 tspan = [0 40];
 mu = 1.5;
 args = {mu};
 
-for h=hs
-    [T,X] = EulerExplicit_fixed(@VanderPol, tspan, h, x0, args);
+figure('Renderer', 'painters', 'Position', [10 10 1200 533])
 
-    figure()
-    plot(T,X(1,:));
-    hold on
-    plot(T,X(2,:));
-    xlabel('time')
-    ylabel('x(t)')
+count = 0;
+for h=hs
+    count = count+1;
+    [T,X] = EulerExplicit_fixed(@VanderPol, tspan, h, x0, args);
     
-    set(gcf,'Units','Inches');
-    pos = get(gcf,'Position');
-    set(gcf,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
-    print(gcf,'2_4_vdp_mu_1_5_h_' + strrep(string(h),'.','_')+'_T','-dpdf','-r0')
-    
-    figure()
-    plot(X(1,:), X(2,:));
-    xlabel('x_1');
+    subplot(2,2,1)
+    if any(slashed_h == count)
+        plot(T,X(1,:),'--');
+        hold on
+    else
+        plot(T,X(1,:));
+        hold on
+    end
+    xlabel('t');
     ylabel('x_2');
     
-    set(gcf,'Units','Inches');
-    pos = get(gcf,'Position');
-    set(gcf,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
-    print(gcf,'2_4_fixed_vdp_mu_1_5_h_' + strrep(string(h),'.','_')+'_PP','-dpdf','-r0')
+    subplot(2,2,3)
+    if any(slashed_h == count)
+        plot(T,X(2,:),'--');
+        hold on
+    else
+        plot(T,X(2,:));
+        hold on
+    end
+    xlabel('t');
+    ylabel('x_2');
+    
+    subplot(2,2,[2,4])
+    if any(slashed_h == count)
+        plot(X(1,:), X(2,:),'--');
+        hold on
+    else
+        plot(X(1,:), X(2,:));
+        hold on
+    end
+    xlabel('x_1');
+    ylabel('x_2');
+    legend(h_legend,'Location','SouthEast')
 end
+
+set(gcf,'Units','Inches');
+pos = get(gcf,'Position');
+set(gcf,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+print(gcf,'2_4_fixed_mu_1_5','-dpdf','-r0')
 
 %% Explicit Fixed Van der Pol mu=15
 clc
 clear
 close all
 
-x0 = [1;1];
-hs = [0.05 0.01 0.001];
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% TIME-STEP SELECTION
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+hs = [0.01 0.005 0.001 0.0001];
+slashed_h = [4];
 
+
+h_legend = [];
+for h=hs
+    h_legend = [h_legend, 'h = '+string(h)];
+end
+
+x0 = [1;1];
 tspan = [0 100];
 mu = 15;
 args = {mu};
 
-for h=hs
-    [T,X] = EulerExplicit_fixed(@VanderPol, tspan, h, x0, args);
+figure('Renderer', 'painters', 'Position', [10 10 1200 533])
 
-    figure()
-    plot(T,X(1,:));
-    hold on
-    plot(T,X(2,:));
-    xlabel('time')
-    ylabel('x(t)')
+count = 0;
+for h=hs
+    count = count+1;
+    [T,X] = EulerExplicit_fixed(@VanderPol, tspan, h, x0, args);
     
-    set(gcf,'Units','Inches');
-    pos = get(gcf,'Position');
-    set(gcf,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
-    print(gcf,'2_4_fixed_vdp_mu_15_h_' + strrep(string(h),'.','_')+'_T','-dpdf','-r0')
-    
-    figure()
-    plot(X(1,:), X(2,:));
-    xlabel('x_1');
+    subplot(2,2,1)
+    if any(slashed_h == count)
+        plot(T,X(1,:),'--');
+        hold on
+    else
+        plot(T,X(1,:));
+        hold on
+    end
+    xlabel('t');
     ylabel('x_2');
     
-    set(gcf,'Units','Inches');
-    pos = get(gcf,'Position');
-    set(gcf,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
-    print(gcf,'2_4_vdp_mu_15_h_' + strrep(string(h),'.','_')+'_PP','-dpdf','-r0')
+    subplot(2,2,3)
+    if any(slashed_h == count)
+        plot(T,X(2,:),'--');
+        hold on
+    else
+        plot(T,X(2,:));
+        hold on
+    end
+    xlabel('t');
+    ylabel('x_2');
+    
+    subplot(2,2,[2,4])
+    if any(slashed_h == count)
+        plot(X(1,:), X(2,:),'--');
+        hold on
+    else
+        plot(X(1,:), X(2,:));
+        hold on
+    end
+    xlabel('x_1');
+    ylabel('x_2');
+    legend(h_legend,'Location','SouthEast')
 end
 
-%% Explicit Adaptive Van der Pol
+set(gcf,'Units','Inches');
+pos = get(gcf,'Position');
+set(gcf,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+print(gcf,'2_4_fixed_mu_15','-dpdf','-r0')
+
+%% Explicit Adaptive Van der Pol mu=1.5
 clc
 clear
 close all
 
 tspan = [0 40];
-h0 = 0.5;
+h0 = 0.1;
 x0 = [1;1];
-abstol = 1e-5;
-reltol = 1e-4;
 mu = 1.5;
 args = {mu};
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% TOLERANCE SELECTION
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+tols =  [5e-2, 5e-3, 1e-4, 1e-6];
+slashed_tol = [4];
 
-[T,X] = EulerExplicit_adaptive(@VanderPol, tspan, h0, x0, abstol, reltol, args);
+tols_legend = [];
 
-figure()
-subplot(2,1,1);
-plot(T,X(1,:));
-xlabel('t');
-ylabel('x_1');
-subplot(2,1,2);
-plot(T,X(2,:));
-xlabel('t');
-ylabel('x_2');
+for tol=tols
+    tols_legend = [tols_legend, 'tol = ' + string(tol)];
+end
 
-% set(gcf,'Units','Inches');
-% pos = get(gcf,'Position');
-% set(gcf,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
-% print(gcf,'2_4_vdp_mu_1_5_h_' + strrep(string(h),'.','_')+'_T','-dpdf','-r0')
+figure('Renderer', 'painters', 'Position', [10 10 1200 800])
 
-figure()
-plot(X(1,:), X(2,:));
-xlabel('x_1');
-ylabel('x_2');
+count = 0;
+infos = zeros(4,size(tols,2));
 
-% set(gcf,'Units','Inches');
-% pos = get(gcf,'Position');
-% set(gcf,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
-% print(gcf,'2_4_fixed_vdp_mu_1_5_h_' + strrep(string(h),'.','_')+'_PP','-dpdf','-r0')
+for tol=tols
+    count = count + 1;
+    abstol = tol;
+    reltol = tol;
+    
+    [T,X,r_out,h_out,info] = EulerExplicit_adaptive(@VanderPol, tspan, h0, x0, abstol, reltol, args);
+    infos(:,count) = info;
+
+    subplot(3,2,1);
+    if any(slashed_tol == count)
+        plot(T,X(1,:),'--');
+        hold on
+    else
+        plot(T,X(1,:));
+        hold on
+    end
+    xlabel('t');
+    ylabel('x_1');
+
+    subplot(3,2,3);
+    if any(slashed_tol == count)
+        plot(T,X(2,:),'--');
+        hold on
+    else
+        plot(T,X(2,:));
+        hold on
+    end
+    xlabel('t');
+    ylabel('x_2');
+
+    subplot(3,2,[2,4]);
+    if any(slashed_tol == count)
+        plot(X(1,:), X(2,:),'--');
+        hold on
+    else
+        plot(X(1,:), X(2,:));
+        hold on
+    end
+    xlabel('x_1');
+    ylabel('x_2');
+    xlim([-2.5 2.5])
+    legend([tols_legend],'Location','SouthEast');
+
+    subplot(3,2,5);
+    if any(slashed_tol == count)
+        plot(T(1:end-1),h_out,'--');
+        hold on
+    else
+        plot(T(1:end-1),h_out);
+        hold on
+    end
+    xlabel('t');
+    ylabel('h');
+    ylim([-0.02 0.35])
+
+    subplot(3,2,6);
+    if any(slashed_tol == count)
+        plot(T(1:end-1),r_out,'--');
+        hold on
+    else
+        plot(T(1:end-1),r_out);
+        hold on
+    end
+    plot(tspan, [1 1],'r');
+    xlabel('t');
+    ylabel('r');
+    ylim([0 1.2]);
+end
+
+set(gcf,'Units','Inches');
+pos = get(gcf,'Position');
+set(gcf,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+print(gcf,'2_4_adaptive_mu_1_5','-dpdf','-r0')
+
+infos_csv = [tols;infos];
+csvwrite('2_4_adaptive_mu_1_5.csv',infos_csv,0,1);
+
+%% Explicit Adaptive Van der Pol mu=15
+clc
+clear
+close all
+
+tspan = [0 100];
+h0 = 0.1;
+x0 = [1;1];
+mu = 15;
+args = {mu};
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% TOLERANCE SELECTION
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+tols =  [5e-2, 5e-3, 1e-4, 1e-6];
+slashed_tol = [4];
+
+tols_legend = [];
+
+for tol=tols
+    tols_legend = [tols_legend, 'tol = ' + string(tol)];
+end
+
+figure('Renderer', 'painters', 'Position', [10 10 1200 800])
+
+count = 0;
+infos = zeros(4,size(tols,2));
+
+for tol=tols
+    count = count + 1;
+    abstol = tol;
+    reltol = tol;
+    
+    [T,X,r_out,h_out,info] = EulerExplicit_adaptive(@VanderPol, tspan, h0, x0, abstol, reltol, args);
+    infos(:,count) = info;
+
+    subplot(3,2,1);
+    if any(slashed_tol == count)
+        plot(T,X(1,:),'--');
+        hold on
+    else
+        plot(T,X(1,:));
+        hold on
+    end
+    xlabel('t');
+    ylabel('x_1');
+
+    subplot(3,2,3);
+    if any(slashed_tol == count)
+        plot(T,X(2,:),'--');
+        hold on
+    else
+        plot(T,X(2,:));
+        hold on
+    end
+    xlabel('t');
+    ylabel('x_2');
+
+    subplot(3,2,[2,4]);
+    if any(slashed_tol == count)
+        plot(X(1,:), X(2,:),'--');
+        hold on
+    else
+        plot(X(1,:), X(2,:));
+        hold on
+    end
+    xlabel('x_1');
+    ylabel('x_2');
+    xlim([-2.5 2.5])
+    legend([tols_legend],'Location','SouthEast');
+
+    subplot(3,2,5);
+    if any(slashed_tol == count)
+        plot(T(1:end-1),h_out,'--');
+        hold on
+    else
+        plot(T(1:end-1),h_out);
+        hold on
+    end
+    xlabel('t');
+    ylabel('h');
+    ylim([-0.02 0.25])
+
+    subplot(3,2,6);
+    if any(slashed_tol == count)
+        plot(T(1:end-1),r_out,'--');
+        hold on
+    else
+        plot(T(1:end-1),r_out);
+        hold on
+    end
+    plot(tspan, [1 1],'r');
+    xlabel('t');
+    ylabel('r');
+    ylim([0 1.2]);
+end
+
+
+set(gcf,'Units','Inches');
+pos = get(gcf,'Position');
+set(gcf,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+print(gcf,'2_4_adaptive_mu_15','-dpdf','-r0')
+
+infos_csv = [tols;infos];
+csvwrite('2_4_adaptive_mu_15.csv',infos_csv,0,1);
 
